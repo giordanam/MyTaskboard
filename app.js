7679a362dfb22ed7357fe946afe26af773674986
Martucci <giordana.martucci@intern.datalogic.com>
2026-09-22 17:42:20 +0200

    added (again) id on the buttons

---
+++ app.js
@@ -0,0 +1,4 @@
+//recupero variabili tramite id dell'html
+const btnNewActivity = document.getElementById("btn-new-act")
+const btnCleanFilters = document.getElementById("btn-clean-filters")
+const gridCards = document.getElementById("grid-cards")
